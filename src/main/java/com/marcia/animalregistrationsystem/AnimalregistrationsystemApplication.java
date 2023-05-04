b450d3db879a03c512892c3119d89c992a7880c1
MarciaMadrugaBarros <MarciaMadrugaBarros>
2023-05-04 00:48:27 -0300

    Project create

---
+++ src/main/java/com/marcia/animalregistrationsystem/AnimalregistrationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.marcia.animalregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalregistrationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalregistrationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcia/animalregistrationsystem/AnimalregistrationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcia.animalregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalregistrationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
