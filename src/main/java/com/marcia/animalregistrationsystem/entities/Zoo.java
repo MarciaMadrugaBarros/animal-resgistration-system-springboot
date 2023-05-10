aa7762c811aec8cfd8936b40964bb52b8108ad9c
MarciaMadrugaBarros <MarciaMadrugaBarros>
2023-05-10 15:42:13 -0300

    Adicionando e removendo, usando o set

--- src/main/java/com/marcia/animalregistrationsystem/entities/Zoo.java
+++ src/main/java/com/marcia/animalregistrationsystem/entities/Zoo.java
@@ -1,5 +1,29 @@
 package com.marcia.animalregistrationsystem.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Zoo {
 
+	public void addAnimal(Animal animal) {
+		
+				//HashSet, que é a implementação mais rapida
+				Set<String> set = new HashSet<>();
+				
+				//Adiciona os animais
+				set.add("Dog");
+				set.add("Cat");
+				set.add("Bird");
+				set.add("Lizard");
+				set.add("Shark");
+		
+				//Remove os animais
+				set.remove("Bird");
+				set.remove("Lizard");
+				
+				//Imprime na tela os animais adicionados
+				for (String p : set) {
+					System.out.println(p);
+					}
+	}
 }
